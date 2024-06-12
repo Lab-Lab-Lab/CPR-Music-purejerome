085e7b51843e67b4a5a8529e804722ce95cb6beb
Jerome <donfacjc@dukes.jmu.edu>
2024-06-12 16:45:11 -0400

    grid landing layout

--- components/nav.js
+++ components/nav.js
@@ -1,4 +1,3 @@
-'use client';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
@@ -8,17 +7,27 @@ import { useQuery } from 'react-query';
 import LoginOut from './loginout';
 import { getEnrollments } from '../api';
 import CourseSelector from './courseSelector';
-import {NavActivityPicker} from './student/navActivityPicker';
+import { NavActivityPicker } from './student/navActivityPicker';
 import { PiecePicker } from './student/piecePicker';
 
 function Navigation() {
   const router = useRouter();
   const { slug, piece } = router.query;
-  const { isLoading, error, data: enrollments } = useQuery('enrollments', getEnrollments, {staleTime: 5 * 60 * 1000})
+  const {
+    isLoading,
+    error,
+    data: enrollments,
+  } = useQuery('enrollments', getEnrollments, { staleTime: 5 * 60 * 1000 });
   // const currentEnrollment = slug && enrollments && enrollments.filter((elem) => elem.course.slug === slug)[0]
-  
+
   return (
-    <Navbar bg="primary" variant="dark" expand="sm">
+    <Navbar
+      bg="primary"
+      variant="dark"
+      expand="sm"
+      sticky="top"
+      style={{ zIndex: '3' }}
+    >
       <Container fluid>
         <Link href="/" passHref>
           <Navbar.Brand>MusicCPR</Navbar.Brand>
@@ -26,13 +35,15 @@ function Navigation() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            {enrollments ? <CourseSelector /> : 
+            {enrollments ? (
+              <CourseSelector />
+            ) : (
               <Nav.Item>
                 <Link href="/courses" passHref legacyBehavior>
                   <Nav.Link>Courses</Nav.Link>
                 </Link>
               </Nav.Item>
-            }
+            )}
             {slug && <PiecePicker />}
             {piece && <NavActivityPicker />}
           </Nav>
