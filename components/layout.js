122b164e715fd7590b52dbb4f6f8bdc7d29c1261
Jerome <donfacjc@dukes.jmu.edu>
2024-07-09 12:10:19 -0400

    intial layout changes

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,3 @@
-'use client';
-import Container from 'react-bootstrap/Container';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { useSession } from 'next-auth/react';
@@ -9,62 +7,92 @@ import Navigation from './nav';
 import styles from './layout.module.css';
 import { getUserProfile, gotUser } from '../actions';
 import { Spinner } from 'react-bootstrap';
+import React, { Fragment } from 'react';
+import Container from 'react-bootstrap/Container';
 
 const PUBLIC_PATHS = ['/', '/about', '/auth/signin', '/api/auth/signout'];
 
-export default function Layout({ children }) {
+export default function Layout({ children, noPadding = false }) {
   const router = useRouter();
 
   const { status, data } = useSession({
     required: !PUBLIC_PATHS.includes(router.pathname),
   });
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   useEffect(() => {
-    if (status === "authenticated") {
-      dispatch(gotUser({user:data.user, token: data.djangoToken}))
-      dispatch(getUserProfile({token: data.djangoToken}))
+    if (status === 'authenticated') {
+      dispatch(gotUser({ user: data.user, token: data.djangoToken }));
+      dispatch(getUserProfile({ token: data.djangoToken }));
     }
-  },[status, dispatch]);
+  }, [status, dispatch]);
 
-  const {loaded: userLoaded, token } = useSelector(state => state.currentUser)
+  const { loaded: userLoaded, token } = useSelector(
+    (state) => state.currentUser
+  );
   return (
     <>
       <Head>
         <link rel="icon" href="/favicon-16x16.png" />
 
         <title>MusicCPR - Create, Perform, Respond, and Connect</title>
-        <meta name="description" content="MusicCPR facilitates music teachers' collection of individual student achievement data that aligns with ensemble repertoire and artistic processes." />
+        <meta
+          name="description"
+          content="MusicCPR facilitates music teachers' collection of individual student achievement data that aligns with ensemble repertoire and artistic processes."
+        />
 
         <meta property="og:url" content="/" />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="MusicCPR - Create, Perform, Respond, and Connect" />
-        <meta property="og:description" content="MusicCPR facilitates music teachers' collection of individual student achievement data that aligns with ensemble repertoire and artistic processes." />
+        <meta
+          property="og:title"
+          content="MusicCPR - Create, Perform, Respond, and Connect"
+        />
+        <meta
+          property="og:description"
+          content="MusicCPR facilitates music teachers' collection of individual student achievement data that aligns with ensemble repertoire and artistic processes."
+        />
         <meta property="og:image" content="/MusicCPR-logo.png" />
 
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:domain" content="musiccpr.org" />
         <meta property="twitter:url" content="/" />
-        <meta name="twitter:title" content="MusicCPR - Create, Perform, Respond, and Connect" />
-        <meta name="twitter:description" content="MusicCPR facilitates music teachers' collection of individual student achievement data that aligns with ensemble repertoire and artistic processes." />
+        <meta
+          name="twitter:title"
+          content="MusicCPR - Create, Perform, Respond, and Connect"
+        />
+        <meta
+          name="twitter:description"
+          content="MusicCPR facilitates music teachers' collection of individual student achievement data that aligns with ensemble repertoire and artistic processes."
+        />
         <meta name="twitter:image" content="/MusicCPR-logo.png" />
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossorigin
+        ></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Libre+Franklin:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
       <Navigation />
       {(!PUBLIC_PATHS.includes(router.pathname) && userLoaded && token) ||
       PUBLIC_PATHS.includes(router.pathname) ? (
-        <Container fluid>
-          <main className={styles.container}>{children}</main>
+        // <>{children}</>
+        <Container fluid style={noPadding ? { padding: 0 } : undefined}>
+          <main>{children}</main>
         </Container>
       ) : (
-          <Spinner
-            as="span"
-            animation="border"
-            size="sm"
-            role="status"
-            aria-hidden="true"
-            variant="primary"
-          >
-            <span className="visually-hidden">Loading...</span>
-          </Spinner>
+        <Spinner
+          as="span"
+          animation="border"
+          size="sm"
+          role="status"
+          aria-hidden="true"
+          variant="primary"
+        >
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
       )}
     </>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 'use client';
-import { useEffect } from 'react';
+import { useEffect, useRef, forwardRef } from 'react';
 import { useDispatch } from 'react-redux';
 import Link from 'next/link';
 import Card from 'react-bootstrap/Card';
@@ -8,263 +8,160 @@ import Layout from '../components/layout';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { Container } from 'react-bootstrap';
-import { FaComment, FaLaughBeam, FaMicrophone, FaMicrophoneAlt } from 'react-icons/fa';
+import {
+  FaComment,
+  FaLaughBeam,
+  FaMicrophone,
+  FaMicrophoneAlt,
+} from 'react-icons/fa';
 import { FaTools } from 'react-icons/fa';
 import { FaRulerCombined } from 'react-icons/fa';
+import styles from '../styles/index.module.css';
+
+function Section({ children, style, id }) {
+  return (
+    <div className={styles.container} style={style} id={id}>
+      {children}
+    </div>
+  );
+}
+
+function GridContainer({ children, style, id }) {
+  return (
+    <div className={styles.gridContainer} style={style} id={id}>
+      {children}
+    </div>
+  );
+}
+
+function CPRCContainer({ children, style, id, scrollID }) {
+  function scrollTo() {
+    document.getElementById(scrollID).scrollIntoView({
+      behavior: 'smooth',
+    });
+  }
+  return (
+    <div
+      className={styles.cprcContainer}
+      style={style}
+      id={id}
+      onClick={scrollTo}
+    >
+      {children}
+    </div>
+  );
+}
+
+const BackToTop = forwardRef(({ style, id }, ref) => {
+  function scrollTo() {
+    window.scrollTo(0, 0);
+  }
+  return (
+    <button
+      className={styles.backToTopButton}
+      style={style}
+      id={id}
+      ref={ref}
+      onClick={scrollTo}
+    >
+      Back To Top
+    </button>
+  );
+});
+
+function InfoContainer({ children, style, id }) {
+  return (
+    <div className={styles.infoContainer} style={style} id={id}>
+      {children}
+    </div>
+  );
+}
+
+function Desciption({ children, style, id }) {
+  return (
+    <div className={styles.descContainer} style={style} id={id}>
+      <p>{children}</p>
+    </div>
+  );
+}
+
+function SectionHeading({ title, style, id, img }) {
+  // let newStyle = style || {};
+  // newStyle.backgroundImage = `url(${img})`;
+  return (
+    <div className={styles.sectionHeading} style={style} id={id}>
+      <h2>{title}</h2>
+    </div>
+  );
+}
 
 function Index() {
-  return (
-    <Layout>
-      <h1 className="mt-3">Welcome to Music CPR</h1>
-      <Row>
-        <Col>
-          <p>
-            MusicCPR is a free platform that facilitates music
-            teachers' collection of individual student achievement data that
-            aligns with ensemble repertoire and artistic processes (create,
-            perform, respond, connect) described in National Standards for Arts
-            Education.
-          </p>
-        </Col>
-        <Col
-          sm={2}
-          className="d-none d-sm-flex justify-content-center align-items-center"
-        >
-          <FaLaughBeam size="4rem" className="text-info" />
-        </Col>
-      </Row>
+  let button = useRef(null);
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((e) => {
+          if (e.isIntersecting) {
+            if (button.current) {
+              button.current.classList.remove(styles.buttonView);
+            }
+          } else {
+            if (button.current) {
+              button.current.classList.add(styles.buttonView);
+            }
+          }
+        });
+      },
+      {
+        threshold: 0.4,
+      }
+    );
 
-      <Row
-        className="rounded border border-3"
-        style={{ borderRadius: '1rem !important' }}
-      >
-        <Col
-          className="text-white bg-dark rounded rounded-start border"
-          style={{
-            borderTopLeftRadius: '1rem !important',
-            borderBottomLeftRadius: '1rem !important',
-          }}
-        >
-          <h2 className="display-6 mt-3">Students</h2>
-          <ul>
-            <li>Free</li>
-            <li>Web App</li>
-            <li>Connect with the repertoire</li>
-            <li>Perform Activities for pieces assigned by your teacher</li>
-            <li>Compose countermelodies</li>
-            <li>Reflect on your experiences</li>
-          </ul>
-        </Col>
-        <Col className="bg-light">
-          <h2 className="display-6 mt-3">Teachers</h2>
-          <ul>
-            <li>Free</li>
-            <li>Standards-aligned</li>
-            <li>Web-based</li>
-            <li>Assign activities</li>
-            <li>
-              Grade students' work
-              <ul>
-                <li>give individual performance feedback</li>
-                <li>export for use in other tools</li>
-              </ul>
-            </li>
-          </ul>
-        </Col>
-        <Col md="6" className="">
-          <h2 className="display-6 mt-3">Standards-Based Music Education</h2>
-          <p>
-            Teachers who provide their students with a standards-based music
-            education facilitate opportunities for every student to develop
-            skill and knowledge related to creating original music, performing
-            their own and others’ music, responding to music, and connecting in,
-            around, and through music. While MusicCPR is framed through these
-            four artistic processes as conceptualized in National Standards for
-            Arts Education, most states have comparable policy documents that
-            outline what should be part of students’ P-12 music education
-            experiences. MusicCPR is intended to bridge the gap between these
-            policies and common practice by providing a free, research-based
-            resource for any music teacher who wishes to provide their students
-            opportunities to create, perform, respond, and connect through
-            activities grounded in large ensemble repertoire.
-          </p>
-        </Col>
-      </Row>
-      {/* <h2 className="display-6 mt-5">Standards-Based Music Education</h2>
-      <p>
-        Teachers who provide their students with a standards-based music
-        education facilitate opportunities for every student to develop skill
-        and knowledge related to creating original music, performing their own
-        and others’ music, responding to music, and connecting in, around, and
-        through music. While MusicCPR is framed through these four artistic
-        processes as conceptualized in National Standards for Arts Education,
-        most states have comparable policy documents that outline what should be
-        part of students’ P-12 music education experiences. MusicCPR is intended
-        to bridge the gap between these policies and common practice by
-        providing a free, research-based resource for any music teacher who
-        wishes to provide their students opportunities to create, perform,
-        respond, and connect through activities grounded in large ensemble
-        repertoire.
-      </p> */}
-      <Row style={{ borderRadius: '1rem' }} className="mt-5 border p-3">
-        <Col
-          sm={2}
-          className="d-flex justify-content-center align-items-center"
-        >
-          <FaTools size="3rem" className="text-primary" />
-        </Col>
-        <Col>
-          <h3 className="text-primary">Individual Student Assessment Tools</h3>{' '}
-          facilitate music educators' in collecting data on each individual student's
-          achievement relative to four artistic processes (create, perform,
-          respond, connect), and to track and share their growth over time.
-        </Col>
-        {/* <Col>
-          <section>
+    observer.observe(document.getElementById('top'));
+  });
+  return (
+    <Layout noPadding>
+      <Section id="top">
+        <GridContainer>
+          <CPRCContainer scrollID="create">
             <h2>Create</h2>
-            <img
-              src="screenshots/landing/Orchestra - Create.png"
-              alt="Create Activity in MusicCPR"
-              className="float-start"
-              width="450"
-            />
-            <p>
-              The Create Artistic Process is underrepresented in instrumental
-              music education. MusicCPR scaffolds teachers who may be new to
-              teaching composition in instrumental music to including it in
-              their classes. Students add notes to the music score in the
-              assignment's web page and then submit their countermelody along
-              with a recording of them performing it.
-            </p>
-          </section>
-        </Col> */}
-      </Row>
-      <Row
-        style={{ borderRadius: '1rem' }}
-        className="mt-5 bg-dark text-white p-3"
-      >
-        <Col className="text-end">
-          <h3 className="text-warning">
-            Valid And Reliable Assessment Rubrics
-          </h3>{' '}
-          provide students, teachers, parents, and other stakeholders with
-          valuable information about each student’s progress.
-        </Col>
-        <Col
-          sm={2}
-          className="d-flex justify-content-center align-items-center"
-        >
-          <FaRulerCombined size="3rem" className="text-warning" />
-        </Col>
-      </Row>
-      <Row style={{ borderRadius: '1rem' }} className="mt-5 border p-3">
-        <Col
-          sm={2}
-          className="d-flex justify-content-center align-items-center"
-        >
-          <FaTools size="3rem" className="text-info" />
-        </Col>
-        <Col>
-          <h3 className="text-info">Perform Prompts</h3> encourage every student in
-          the ensemble &emdash; regardless of the instrument they play &emdash;
-          to learn important musical elements such as melodies and bass lines.
-          Students will not be restricted to graduating high school music only
-          having learned twelve 3rd clarinet parts; they will be able to play
-          recognizable melodies and develop understanding of the bass lines and
-          harmonies that underpin those themes.
-        </Col>
-      </Row>
-      <Row
-        style={{ borderRadius: '1rem' }}
-        className="mt-5 bg-dark text-white p-3"
-      >
-        <Col className="text-end">
-          <h3 className="text-danger">Create Prompts</h3> facilitate students
-          creating their own original music in the context of ensemble
-          repertoire they are learning to perform. Do you tell your
-          administrators, parents, and other stakeholders that music is
-          creative? Then consider using MusicCPR to help your students do
-          something creative in music class!
-        </Col>
-        <Col
-          sm={2}
-          className="d-flex justify-content-center align-items-center"
-        >
-          <FaMicrophoneAlt size="3rem" className="text-danger" />
-        </Col>
-      </Row>
-      <Row style={{ borderRadius: '1rem' }} className="my-5 border p-3">
-        <Col
-          sm={2}
-          className="d-flex justify-content-center align-items-center"
-        >
-          <FaComment size="3rem" className="text-success" />
-        </Col>
-        <Col>
-          <h3 className="text-success">Respond And Connect Prompts</h3> offer
-          opportunities for students to evaluate their own performances, connect
-          their experiences with those of the composer or larger cultural and
-          historical contexts, and reinforce valuable literacy skills through
-          organizing and sharing their reflections.
-        </Col>
-      </Row>
-      {/* <Row>
-        <Col>
-          <section>
+          </CPRCContainer>
+          <CPRCContainer scrollID="perform">
             <h2>Perform</h2>
-            <img
-              src="screenshots/landing/Orchestra - Melody.png"
-              alt="Perform Activity in MusicCPR"
-              className="float-end"
-              width="450"
-            />
-            <p>
-              In MusicCPR, students participate in the Perform Artistic Process
-              through practicing and then recording their performance of both
-              the melody and then in a separate activity the bassline of the
-              assigned piece.
-            </p>
-          </section>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <section>
+          </CPRCContainer>
+          <CPRCContainer scrollID="respond">
             <h2>Respond</h2>
-            <img
-              src="screenshots/landing/Orchestra - Respond.png"
-              alt="Respond Activity in MusicCPR"
-              className="float-start"
-              width="450"
-            />
-            <p>
-              In MusicCPR, students participate in the Respond Artistic Process
-              through reflecting on their experience with the preceding
-              activities (performing the melody and bassline, and composing and
-              performing their own countermelody).
-            </p>
-          </section>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <section>
+          </CPRCContainer>
+          <CPRCContainer scrollID="connect">
             <h2>Connect</h2>
-            <img
-              src="screenshots/landing/Orchestra - Connect - Green.png"
-              alt="Connect Activity in MusicCPR"
-              className="float-end"
-              width="450"
-            />
-            <p>
-              In MusicCPR, students participate in the Connect Artistic Process
-              through hearing from musicians and/or composers about the piece
-              they're learning and reflecting on how it relates to or contrasts
-              their own lived experiences.
-            </p>
-          </section>
-        </Col>
-      </Row> */}
+          </CPRCContainer>
+        </GridContainer>
+      </Section>
+      <Section id="create" style={{ backgroundColor: 'rgb(255, 126, 126)' }}>
+        <InfoContainer>
+          <SectionHeading title="Create" img="createimg.jpg" />
+          <Desciption>Incubating creativity and artistic talents.</Desciption>
+        </InfoContainer>
+      </Section>
+      <Section id="perform" style={{ backgroundColor: 'rgb(253, 253, 115)' }}>
+        <InfoContainer>
+          <Desciption>Present and display talent in music.</Desciption>
+          <SectionHeading title="Perform" img="performmusic.jpg" />
+        </InfoContainer>
+      </Section>
+      <Section id="respond" style={{ backgroundColor: 'rgb(56, 144, 56)' }}>
+        <InfoContainer>
+          <SectionHeading title="Respond" img="classroom.jpg" />
+          <Desciption>Understand and interpret meaning in music.</Desciption>
+        </InfoContainer>
+      </Section>
+      <Section id="connect" style={{ backgroundColor: 'rgb(78, 78, 255)' }}>
+        <InfoContainer>
+          <Desciption>Relate art with your beliefs and others.</Desciption>
+          <SectionHeading title="Connect" img="teachingmusic.jpg" />
+        </InfoContainer>
+      </Section>
+      <BackToTop ref={button} />
     </Layout>
   );
 }
