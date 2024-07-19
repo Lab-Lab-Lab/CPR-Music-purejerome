57434413695a78d7278003ef1a34769bea2cedd6
Jerome <donfacjc@dukes.jmu.edu>
2024-07-19 14:47:44 -0400

    signout page prettified

--- pages/auth/signout.js
+++ pages/auth/signout.js
@@ -6,27 +6,100 @@ import { useDispatch } from 'react-redux';
 import Layout from '../../components/layout';
 import { logoutUser } from '../../actions';
 import { useRouter } from 'next/router';
+import { useRef, useEffect, useState } from 'react';
 
 function SignOut() {
   const router = useRouter();
+  const container = useRef(null);
+  const [loaded, setLoad] = useState(false);
+  const [widthBool, setWidthBool] = useState(false);
 
   const { data: session } = useSession();
   const dispatch = useDispatch();
   const logout = (ev) => {
     dispatch(logoutUser(session.djangoToken));
     signOut();
-    router.push('/')
+    router.push('/');
   };
+
+  // gets remaining height for the 100vh viewport of site and dynamically size the signout area
+  function getHeightAndWidth() {
+    const rect = document.querySelector('nav').getBoundingClientRect();
+    const { height } = rect;
+    const windowHeight = window.innerHeight;
+    container.current.style.height = `${windowHeight - height}px`;
+
+    const containerWidth = container.current.getBoundingClientRect();
+    const { width } = containerWidth;
+    if (width > 900) {
+      setWidthBool(false);
+    } else {
+      setWidthBool(true);
+    }
+  }
+
+  // check if container is loaded
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (container.current) {
+        setLoad(true);
+        clearInterval(interval);
+      }
+    }, 0);
+  }, []);
+
+  // sets container height to auto fill to the bottom
+  useEffect(() => {
+    if (loaded) {
+      getHeightAndWidth();
+      window.addEventListener('resize', getHeightAndWidth);
+    }
+  }, [loaded]);
   return (
     <Layout>
-      <Row>
-        <Col md={4}>
-          <h1>Are you sure you want to Sign Out?</h1>
-          <Button variant="warning" onClick={logout}>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+        ref={container}
+      >
+        <figure style={{ width: '200px', marginBottom: '40px' }}>
+          <img
+            style={{ width: '100%', height: 'auto' }}
+            src="/MusicCPR-logo.png"
+          />
+        </figure>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-evenly',
+            alignItems: 'center',
+            width: widthBool ? '90%' : '35%',
+            border: 'solid 2px rgba(138, 136, 136, 1)',
+            padding: '20px 0 20px 0',
+            borderRadius: '3px',
+            height: '200px',
+            boxShadow: '0px 0px 20px -3px rgba(0,0,0,0.25)',
+          }}
+        >
+          <h1
+            style={{
+              textAlign: 'center',
+              width: '70%',
+              fontSize: '1.7rem',
+            }}
+          >
+            Are you sure you want to Sign Out?
+          </h1>
+          <Button style={{ width: '70%' }} onClick={logout}>
             Sign out
           </Button>
-        </Col>
-      </Row>
+        </div>
+      </div>
     </Layout>
   );
 }
